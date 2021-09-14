384d2b90d583c9e3b6706a7885952b27162dcd0e
Ayush Sood <ayushsood2@gmail.com>
2021-09-13 23:17:36 -0500

    Added some comments to explain the business logic

--- Scripts/process_data_glue.py
+++ Scripts/process_data_glue.py
@@ -1,132 +1,95 @@
+import sys
 import os
 import boto3
-from flask import Flask,flash,request,redirect,send_file,render_template,Response, url_for
-from werkzeug.utils import secure_filename
 from datetime import datetime
-import time
-import sys
-import subprocess
+from pyspark.sql.window import Window
+from pyspark.sql import SparkSession
+from pyspark.sql import SQLContext
+from pyspark.context import SparkContext
+from awsglue.context import GlueContext
+from awsglue.transforms import *
+from awsglue.job import Job
+from awsglue.utils import getResolvedOptions
 
-thismodule = sys.modules[__name__]
-app = Flask(__name__, template_folder='templates')
-UPLOAD_FOLDER = 'uploads/'
-DOWNLOAD_FOLDER = 'output_file/'
-GlUE_SCRIPT = 'Scripts/process_data_glue.py'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
-# BUCKET_NAME = 'serverless-flask-dev-serverlessdeploymentbucket-1wdb3e4zzsp3'
+#Initialize Glue and Spark Variables
+args = getResolvedOptions(sys.argv, ["JOB_NAME","input_file_path", "role_name"])
+sc = SparkContext()
+glueContext = GlueContext(sc)
+spark = glueContext.spark_session
+job = Job(glueContext)
+job.init(args["JOB_NAME"], args)
 
-s3 = boto3.client('s3')
-glue = boto3.client('glue')
-cf =  boto3.client('cloudformation')
-global job_id
-thismodule.input_file_name = ''
-global output_file_name
-BUCKET_NAME = cf.describe_stacks(StackName='serverless-flask-dev')['Stacks'][0]['Outputs'][2]['OutputValue']
+#Create required services client
+client = boto3.client('glue')
 
-# Upload API
-@app.route("/uploadfile", methods=['GET', 'POST'])
-def upload_file():
-    print(request)
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            print('no file')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            print('no filename')
-            return redirect(request.url)
-        else:
-            # filename = secure_filename(file.filename)
-            # print(filename)
-            tsv = request.files['file']
-            if tsv:
-                thismodule.input_file_name = secure_filename(tsv.filename)
-                print(input_file_name)
-                tsv.save("/tmp/{}".format(input_file_name))
-                s3.upload_file(
-                    Bucket = BUCKET_NAME,
-                    Filename="/tmp/{}".format(input_file_name),
-                    Key = f'input_file/{input_file_name}'
-                )
-                msg = f"Upload Done at {BUCKET_NAME}/input_file/{input_file_name} ! "
-            print("saved file successfully")
+#Get data supplied through input
+path = args["input_file_path"]
+BUCKET_NAME = args['input_file_path'].split('/')[2]
+role_name = args['role_name']
+output_file_path = f"s3://{BUCKET_NAME}/output_file/{datetime.today().strftime('%Y-%m-%d')}_SearchKeywordPerformance.tab.tsv"
+output_file_name = output_file_path.split('/')[-1]
 
-      # send file name as parameter to downlad
-            return render_template('upload_complete.html',msg = msg)
-            # return redirect('/downloadfile/'+ filename)
-    return render_template('file_upload_to_s3.html')
+#Create Spark DataFrame from the input data for processing
+hits_data = spark.read.csv(path,header=True, sep= '\t', inferSchema=True)
+hits_data.createOrReplaceTempView('hits_data')
 
-#Process API
-@app.route("/processfile", methods = ['GET', 'POST'])
-def process_file():
-    print(request)
-    if request.method == 'POST':
-        print('Starting Glue job for processing')
-        job_response = glue.start_job_run(
-                JobName="serverless-flask-dev-ProcessData",
-                Arguments={
-                    # Specify any arguments needed based on bucket and keys (e.g. input/output S3 locations)
-                    "--JOB_NAME": "serverless-flask-dev-ProcessData",
-                    "--input_file_path": f"s3://{BUCKET_NAME}/input_file/{input_file_name}",
-                    "--job-bookmark-option": "job-bookmark-enable",
-                    "--output-file-path": ""
-                }
-            )
-        thismodule.job_id =  job_response['JobRunId']
-        print(job_id)
-        return redirect(url_for('check_job_status'))
+#Business Logic transforming the data using Spark SQL
+# 1. Parition Data by IP Addresss and ordered by timestamp of the hits.
+# 2. Update partitions referrer as the first referrer value for that IP.
+# 3. Parse referrer URL to get HOST and QUERY informationself.
+# 4. QUERY will provide us the SEARCH_KEYWORD using regex.
+# 5. SUM of the Product_list field will give us the REVENUE for that search term.
+# 6. This will be our resulting dataframe.
+# 7. This job can now handle the load of bigger size files by distributing computing.
+revenue_data = spark.sql('''
+                SELECT
+                    SEARCH_ENGINE_DOMAIN,
+                    SEARCH_KEYWORD,
+                    SUM(`total_revenue`) as REVENUE
+                    FROM
+                        (
+                         SELECT
+                             date_time,
+                             ip,
+                             CONCAT(SPLIT(PARSE_URL(`first_referrer`, 'HOST'),'[.]')[1],'.',
+                                     SPLIT(PARSE_URL(`first_referrer`, 'HOST'),'[.]')[2])
+                             as SEARCH_ENGINE_DOMAIN,
+                             REPLACE(LOWER(PARSE_URL(`first_referrer`,'QUERY','[a-z]')),'+',' ') as SEARCH_KEYWORD,
+                             SPLIT(`product_list`,';')[1] as product_name,
+                             CAST(SPLIT(`product_list`,';')[3] as Integer) as total_revenue
+                             FROM
+                             (
+                                 SELECT
+                                     date_time,
+                                     ip,
+                                     product_list,
+                                     FIRST_VALUE(referrer)over (PARTITION BY ip ORDER BY date_time ASC) as first_referrer
+                                     FROM hits_data
+                                )
+                            )
+                            GROUP BY
+                                SEARCH_KEYWORD,
+                                SEARCH_ENGINE_DOMAIN
+                            ORDER BY
+                                REVENUE DESC
+                                ''')
 
-    return render_template('processing_file.html')
+#Convert Resulting Spark DF to Pandas DF to get single csv file as output in an S3 bucket without using repartitioning
+revenue_data.toPandas().to_csv(output_file_path, sep = '\t',index = False)
 
-#Get Status API
-@app.route("/checkjobstatus", methods = ['GET', 'POST'])
-def check_job_status():
-    print(request)
-    if request.method == 'GET':
-        response = glue.get_job_run(
-            JobName = 'serverless-flask-dev-ProcessData',
-            RunId = job_id
-            )
-        status = response['JobRun']['JobRunState']
-        if status == 'SUCCEEDED':
-            print('Job Finished Successfully')
-            return redirect(url_for('download_file'))
-        else:
-            print(f"job is still running with {status}")
-            # time.sleep(20)
-            return render_template('processing_file.html')
-            # return redirect(url_for('check_job_status'))
-    return render_template('processing_file.html')
+#Update Job Input parameter with Output File name which will be passed to API to be available for download.
+response = client.update_job(
+    JobName='process_data',
+    JobUpdate={
+        'Description': 'updating arguments',
+        'Role': f'{role_name}',
+        'Command': {
+            'Name': 'glueetl',
+            'ScriptLocation': 's3://{BUCKET_NAME}/Scripts/process_data_glue.py'
+        },
+        'DefaultArguments': {
+            '--output-file-name': f'{output_file_name}'
+    }
+)
 
-@app.route("/downloadfile", methods = ['GET', 'POST'])
-def download_file():
-    thismodule.output_file_name= glue.get_job(JobName= 'process_data')['Job']['DefaultArguments']['--output-file-name']
-    print(output_file_name)
-    msg = f"File Processed and uploaded at {BUCKET_NAME}/output_file/{output_file_name} ! "
-    return render_template('downloading_file.html', msg = msg)
-
-@app.route("/download", methods = ['GET', 'POST'])
-def download():
-    s3_client = boto3.resource('s3')
-    file_name = f'output_file/{output_file_name}'
-    file_path = f'/tmp/{output_file_name}'
-    print(f'output is {output_file_name}')
-    output = s3_client.meta.client.download_file(BUCKET_NAME,file_name , file_path)
-    return send_file(file_path, as_attachment= True, attachment_filename=output_file_name)
-
-def upload_scripts():
-    s3.upload_file(Bucket = BUCKET_NAME,
-                    Filename = GlUE_SCRIPT ,
-                     Key = GlUE_SCRIPT)
-
-@app.route('/', methods=['GET', 'POST'])
-def homepage():
-    upload_scripts()
-    return redirect(url_for('upload_file'))
-
-if __name__ == "__main__":
-    app.run()
+job.commit()
